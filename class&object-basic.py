fad5589f57b64ed2d00a7c6dfac63b4b773fd733
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-08-30 12:27:37 +0530

    Create class&object-basic.py

---
+++ class&object-basic.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, name, roll, std, sec): .. CONST
+        self.name = name
+        self.roll = roll
+        self.std = std
+        self.sec = sec
+        
+    def show_info(self): //METHOD
+        print(f"student name : {self.name} , roll no. : {self.roll} , std : {self.std} , sec : {self.sec}")
+        
+student1 = Student("Aman" , 24, "||", "E") // ACTUAL OBJECT
+student2 = Student("Kriti" , 11, "||", "E")
+
+student1.show_info() //CALL
+student2.show_info()
