972bfc9c6d6f345868f2e5856752136b0a4e0e7c
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-08-31 02:06:17 +0530

    Update class&object-basic.py

--- class&object-basic.py
+++ class&object-basic.py
@@ -8,8 +8,8 @@ class Student:
     def show_info(self): //METHOD
         print(f"student name : {self.name} , roll no. : {self.roll} , std : {self.std} , sec : {self.sec}")
         
-student1 = Student("Aman" , 24, "||", "E") // ACTUAL OBJECT
-student2 = Student("Kriti" , 11, "||", "E")
+student1 = Student("Aman" , 3, "||", "E") // ACTUAL OBJECT
+student2 = Student("Kriti" , 12, "||", "E")
 
 student1.show_info() //CALL
 student2.show_info()
